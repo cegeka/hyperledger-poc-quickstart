a4547ac77a1a6480aae393691d0751e80cb0b85d
Wietse van Brug <wietse.vanbrug@cegeka.com>
2018-09-21 13:48:53 +0200

    add password to user setup

--- server/setup/setup.js
+++ server/setup/setup.js
@@ -6,14 +6,14 @@ var request = require('request-promise');
 var rootUrl = 'http://localhost:3000/api/';
 
 /**
- * Create user admin
+ * Create admin user function
 */
 function createAdmin() {
     var options = {
         method: 'POST',
         uri: rootUrl + 'Admin',
         body: {
-            userId: 'admin-1',
+            userId: 'admin',
             name: 'Standard Admin'
         },
         json: true
@@ -28,14 +28,15 @@ function createAdmin() {
 }
 
 /**
- * Create a customer
+ * Create customer user function
  */
-function createCustomer(customerId, firstName, lastName) {
+function createCustomer(customerId, password, firstName, lastName) {
     var options = {
         method: 'POST',
         uri: rootUrl + 'Customer',
         body: {
             customerId: customerId,
+            password: password,
             firstName: firstName,
             lastName: lastName
         },
@@ -49,7 +50,10 @@ function createCustomer(customerId, firstName, lastName) {
     });
 }
 
+/**
+ * Execute functions
+ */
 createAdmin();
-createCustomer('customer1', 'First', 'Customer');
-createCustomer('customer2', 'Second', 'Customer');
-createCustomer('customer3', 'Third', 'Customer');
+createCustomer('customer1', 'DEA26157FA355301663174EAC368538CFF8939F36681D6712DEDBA439AB98B70', 'First', 'Customer');
+createCustomer('customer2', 'C8C7CB5B9E8F7A1B3D1D02602ADA62327132391DBE0E8EE07913CD550EEA1F3B', 'Second', 'Customer');
+createCustomer('customer3', '18C5C9BE898C65C5E5C51AC3E94FEACFF0B991F8463A3A18EB524E9F7E6131A8', 'Third', 'Customer');
